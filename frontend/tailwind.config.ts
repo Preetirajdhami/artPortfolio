22116adf44757563327e03fed4069e1c476603cc
preeti <preetirajdhami@gmail.com>
2025-03-29 22:08:17 +0545

    working in controllers

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}', // Adjust this based on your project structure
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
