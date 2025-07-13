909ed09f4f7b04e7764116e3a69c8a20da03caea
preeti <preetirajdhami@gmail.com>
2025-07-13 23:47:05 +0545

    removed metadata

--- frontend/src/app/gallery/[category]/page.tsx
+++ frontend/src/app/gallery/[category]/page.tsx
@@ -1,15 +1,18 @@
+"use server";
 
 import GalleryClientWrapper from "./GalleryClientWrapper";
 
-interface PageProps {
+interface Props {
   params: {
     category: string;
   };
 }
 
-const Page = ({ params }: PageProps) => {
-  const { category } = params;
+const Page = async ({ params }: Props) => {
+  // Await params to satisfy Next.js requirement:
+  const { category } = await Promise.resolve(params);
 
+  // Now pass category to the client wrapper:
   return <GalleryClientWrapper category={category} />;
 };
 
